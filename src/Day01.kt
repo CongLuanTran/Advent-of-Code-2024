195d7eb5af6a3737336ad551e6a592c8736f8d64
Cong Luan Tran <152266452+CongLuanTran@users.noreply.github.com>
2024-12-03 16:19:41 +0700

    Finished Day 1 of Advent of code

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,36 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val a = mutableListOf<Int>()
+        val b = mutableListOf<Int>()
+
+        input.map {
+            val arr = it.split("\\s+".toRegex())
+            a.add(arr[0].toInt())
+            b.add(arr[1].toInt())
+        }
+
+        a.sort()
+        b.sort()
+        var total = 0
+        for(i in input.indices) {
+           total += abs(a[i] - b[i])
+        }
+        return total
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val a = mutableListOf<Int>()
+        val b = mutableMapOf<Int, Int>()
+
+        input.map {
+            val arr = it.split("\\s+".toRegex())
+            a.add(arr[0].toInt())
+            b[arr[1].toInt()] = b.getOrDefault(arr[1].toInt(), 0) + 1
+        }
+        return a.sumOf { it * b.getOrDefault(it, 0) }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
