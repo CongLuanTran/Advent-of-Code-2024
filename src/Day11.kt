a87de191832eab9358e218a648dac310352abc03
CongLuanTran <congluan.tran19@gmail.com>
2024-12-11 17:47:00 +0700

    finalize first version of Day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,10 +1,11 @@
 import kotlin.time.measureTime
 
 class Arrangement(stones: String) {
-    var arrangement = stones.split(" ")
-    var dp = mutableMapOf<Pair<String, Int>, Long>()
+    private var arrangement = stones.split(" ")
 
     fun transform(times: Int): Long {
+        val dp = mutableMapOf<Pair<String, Int>, Long>()
+
         fun blink(stone: String, times: Int): Long {
             if (times == 0) return 1L
             dp[stone to times]?.let { return it }
@@ -12,7 +13,7 @@ class Arrangement(stones: String) {
             return when {
                 stone == "0" -> blink("1", times - 1)
                 stone.length % 2 == 0 -> {
-                    var (left, right) = stone.chunked(stone.length / 2)
+                    val (left, right) = stone.chunked(stone.length / 2)
                         .map { "${it.toLong()}" }
                     blink(left, times - 1) + blink(right, times - 1)
                 }
@@ -20,25 +21,16 @@ class Arrangement(stones: String) {
                 else -> blink("${stone.toLong() * 2024}", times - 1)
             }.also { dp[stone to times] = it }
         }
-
-        fun blinkUp(stone: String, times: Int, length: Long, end: Int): {
-            if (times == end) return length
-            dp[stone to times]?.let{ return it }
-
-            return
-        }
-
         return arrangement.sumOf { blink(it, times) }
     }
 }
 
 fun main() {
     val input = readInput("Day11").first()
-    measureTime() {
+    measureTime {
         Arrangement(input).transform(25).println()
     }.println()
-
-    measureTime() {
+    measureTime {
         Arrangement(input).transform(75).println()
     }.println()
 }
