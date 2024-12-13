7a8f6fe7ebf0171e5c592e600d7df446043f2424
Cong Luan Tran <congluan.tran19@gmail.com>
2024-12-13 17:34:46 +0700

    No, in fact it is a simple linear equation, done

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,37 +1,49 @@
-import kotlin.math.min
+import kotlin.time.measureTime
+
+typealias Point = Pair<Long, Long>
 
 fun main() {
 
-    fun parseLine(line: String): Pair<Int, Int> {
+    fun parseLine(line: String): Point {
         val (x, y) = line.split(":")[1].trim().split(",").map { it.split("""[+=]""".toRegex())[1] }
-        return x.toInt() to y.toInt()
+        return x.toLong() to y.toLong()
     }
 
-    fun optimize(machine: List<String>): Int {
-
-        val a = parseLine(machine[0])
-        val b = parseLine(machine[1])
-        val c = parseLine(machine[2])
-        var dp = MutableList(c.first+1) { MutableList(c.second + 1) { (1e9+1).toInt() } }
-        dp[0][0] = 0
-
-        for (i in 1..c.first) {
-            for (j in 1..c.second) {
-                for (k in listOf(a, b)) {
-                    if (i - k.first >= 0 && j - k.second >= 0) {
-                        dp[i][j] = min(dp[i][j], dp[i - k.first][j - k.second] + (if (k == a) 3 else 1))
-                    }
-                }
-            }
-        }
-
-        return if (dp[c.first][c.second] == (1e9+1).toInt() ) 0 else dp[c.first][c.second]
+    fun getMatrixParam(machine: List<String>): List<Point> {
+        return machine.map { parseLine(it) }
     }
 
-    fun part1(input: List<String>): Int {
+    fun solveLinearEquation(a: Point, b: Point, c: Point): Long {
+        val det = (a.first * b.second - a.second * b.first)
+        val detA = c.first * b.second - c.second * b.first
+        val detB = a.first * c.second - a.second * c.first
+        if (detA % det == 0L && detB % det == 0L)
+            return 3L*detA/det + detB/det
+        return 0L
+    }
+
+    fun part1(input: List<String>): Long {
         val machines = input.filterNot { it.isEmpty() }.chunked(3)
-        return machines.sumOf { optimize(it) }
+        return machines.sumOf {
+            var (a, b, c) = getMatrixParam(it)
+            solveLinearEquation(a, b, c)
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        val add = 10000000000000
+        val machines = input.filterNot { it.isEmpty() }.chunked(3)
+        return machines.sumOf {
+            var (a, b, c) = getMatrixParam(it)
+            c = c.first + add to c.second + add
+            solveLinearEquation(a, b, c)
+        }
     }
     val input = readInput("Day13")
-    part1(input).println()
+    measureTime {
+        part1(input).println()
+    }.println()
+    measureTime {
+        part2(input).println()
+    }.println()
 }
