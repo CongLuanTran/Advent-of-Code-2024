eda59ed38208e8069dd11e3164a511a2e724ec75
Cong Luan Tran <congluan.tran19@gmail.com>
2024-12-17 19:34:59 +0700

    put some typeallias and useful function into Utils

--- src/Utils.kt
+++ src/Utils.kt
@@ -20,9 +20,46 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  */
 fun Any?.println() = println(this)
 
+typealias Map2D = List<MutableList<Char>>
+typealias Point = Pair<Int,Int>
+typealias Input = List<String>
+
 enum class Direction(val x: Int, val y: Int) {
     NORTH(0, -1),
     SOUTH(0, 1),
     EAST(1, 0),
-    WEST(-1, 0)
+    WEST(-1, 0);
+
+    fun turnRight(): Direction =
+        when(this) {
+            NORTH -> EAST
+            EAST -> SOUTH
+            SOUTH -> WEST
+            WEST -> NORTH
+        }
+
+    fun turnLeft(): Direction =
+        when(this) {
+            NORTH -> WEST
+            WEST -> SOUTH
+            SOUTH -> EAST
+            EAST -> NORTH
+        }
+
+    fun asLetter(): Char =
+        when(this) {
+            NORTH -> 'N'
+            SOUTH -> 'S'
+            EAST -> 'E'
+            WEST -> 'W'
+        }
+}
+
+fun Point.move(direction: Direction) =
+    this.first + direction.x to this.second + direction.y
+
+operator fun Map2D.get(point: Point) = this[point.second][point.first]
+
+operator fun Map2D.set(point: Point, char: Char) {
+    this[point.second][point.first] = char
 }
